ffcb67557a5184a1c92be36c1e01e6596f9788ac
Julio Bueso <julio.bueso@allasrepuestos.com>
2019-12-13 15:15:42 -0600

    Primer commit

---
+++ src/app/shared/breadcrumbds/breadcrumbds.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrumbds',
+  templateUrl: './breadcrumbds.component.html',
+  styleUrls: ['./breadcrumbds.component.css']
+})
+export class BreadcrumbdsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
